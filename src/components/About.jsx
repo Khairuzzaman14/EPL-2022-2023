3bd55f0087df17959b48136bbc0719b6be6a12e4
Khairuzzaman <kzamanbm@gmail.com>
2023-04-26 23:14:24 +0800

    completed about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./About.module.css";
+import Henry from "./henry-trophy.jpg..webp";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.about}>
+      <div className="article">
+        This app is for football fans that wants more data regarding Premier
+        League teams' matches.
+      </div>
+      <br />
+      <div className={styles.coyg}>COYG</div>
+      <br />
+      <img src={Henry} />
+    </div>
+  );
 };
 
 export default About;
